20a377672aa607a090091747c949b412d70b5ed8
Xavier Tristancho <xavi@letitguide.com>
2016-02-25 19:19:46 +0100

    Added JSON serialization

--- inventory/Modules/Articles/Models/Article.swift
+++ inventory/Modules/Articles/Models/Article.swift
@@ -8,12 +8,12 @@
 
 import Foundation
 import RealmSwift
+import ObjectMapper
 
-class Article: Object {
+class Article: Object, Mappable {
     
     dynamic var id            = 0
-    let family                = Family()
-    let provider              = Provider()
+    dynamic var family        : Family? = nil
     dynamic var name          = ""
     dynamic var reference     = ""
     dynamic var purchasePrice = 0
@@ -22,4 +22,26 @@ class Article: Object {
     dynamic var quantity      = 0
     dynamic var createdAt     = NSDate(timeIntervalSince1970: 1)
     dynamic var updatedAt     = NSDate(timeIntervalSince1970: 1)
+    
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+    
+    required convenience init?(_ map: Map) {
+        self.init()
+    }
+    
+    func mapping(map: Map) {
+        id            <- map["id"]
+        family        <- map["family"]
+//        provider      <- map["provider"]
+        name          <- map["name"]
+        reference     <- map["reference"]
+        purchasePrice <- map["purchase_price"]
+        costPrice     <- map["cost_price"]
+        sellPrice     <- map["sell_price"]
+        quantity      <- map["quantity"]
+        createdAt     <- map["created_at"]
+        updatedAt     <- map["updated_at"]
+    }
 }
