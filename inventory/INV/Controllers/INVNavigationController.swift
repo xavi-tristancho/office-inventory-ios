911154049aacf778cd8ab05fce5d7142ef89553e
Xavier Tristancho <xavi@letitguide.com>
2016-02-27 01:36:04 +0100

    Added Back Button to INVNavigationController

--- inventory/INV/Controllers/INVNavigationController.swift
+++ inventory/INV/Controllers/INVNavigationController.swift
@@ -13,8 +13,23 @@ class INVNavigationController: UINavigationController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setUpTitle()
+        setUpBackButtonImage()
+    }
+    
+    func setUpTitle(){
         navigationBar.barTintColor = UIColor(hexString: "#47b8e0")
         let titleFont = UIFont(name: "Avenir-Roman", size: 22)
-        navigationBar.titleTextAttributes = [NSFontAttributeName: titleFont!]        
+        navigationBar.titleTextAttributes = [NSFontAttributeName: titleFont!]
+    }
+    
+    func setUpBackButtonImage(){
+        let backButton = UIBarButtonItem(image:UIImage(named: "back-button"), style:.Plain, target:self, action: "popViewController");
+        backButton.tintColor = UIColor(hexString: "ffc952")
+        navigationItem.leftBarButtonItem = backButton
+    }
+    
+    func popViewController(){
+        self.navigationController?.popViewControllerAnimated(true)
     }
 }
