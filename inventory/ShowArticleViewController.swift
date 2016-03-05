4711c675ac72ebd5dd2df0dcb8125c5a79cbc9c1
Xavier Tristancho <xavi@letitguide.com>
2016-03-05 14:08:36 +0100

    Check if article is nil, ipad bug

--- inventory/ShowArticleViewController.swift
+++ inventory/ShowArticleViewController.swift
@@ -16,55 +16,58 @@ class ShowArticleViewController: FormViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        form  +++=
+        
+        if (article != nil)
+        {
+            form  +++=
+                
+                LabelRow("family") {
+                    $0.title = "Familia"
+                    $0.value = article!.family?.name
+                }
+                
+                <<< LabelRow("provider") {
+                    $0.title = "Proveedor"
+                    $0.value = article!.provider?.name
+                }
+                
+                <<< TextRow("name") {
+                    $0.title = "Nombre"
+                    $0.value = article!.name
+                }
+                
+                <<< TextRow("reference") {
+                    $0.title = "Referencia"
+                    $0.value = article!.reference
+                }
+                
+                <<< DecimalRow("purchase_price") {
+                    $0.title = "Precio de Compra"
+                    $0.value = article!.purchasePrice
+                }
+                
+                <<< DecimalRow("cost_price") {
+                    $0.title = "Precio de Coste"
+                    $0.value = article!.costPrice
+                }
+                
+                <<< DecimalRow("sell_price") {
+                    $0.title = "Precio de Venta"
+                    $0.value = article!.sellPrice
+                }
+                
+                <<< DecimalRow("quantity") {
+                    $0.title = "Cantidad"
+                    $0.value = article!.quantity
+            }
             
-        LabelRow("family") {
-            $0.title = "Familia"
-            $0.value = article!.family?.name
+            form +++ Section("")
+                
+                <<< ButtonRow("Rows") {
+                    $0.title = "Guardar"
+                    $0.onCellSelection(didTapSaveButton)
+            }
         }
-            
-        <<< LabelRow("provider") {
-            $0.title = "Proveedor"
-            $0.value = article!.provider?.name
-        }
-            
-        <<< TextRow("name") {
-            $0.title = "Nombre"
-            $0.value = article!.name
-        }
-        
-        <<< TextRow("reference") {
-            $0.title = "Referencia"
-            $0.value = article!.reference
-        }
-        
-        <<< DecimalRow("purchase_price") {
-            $0.title = "Precio de Compra"
-            $0.value = article!.purchasePrice
-        }
-
-        <<< DecimalRow("cost_price") {
-            $0.title = "Precio de Coste"
-            $0.value = article!.costPrice
-        }
-        
-        <<< DecimalRow("sell_price") {
-            $0.title = "Precio de Venta"
-            $0.value = article!.sellPrice
-        }
-        
-        <<< DecimalRow("quantity") {
-            $0.title = "Cantidad"
-            $0.value = article!.quantity
-        }
-        
-        form +++ Section("")
-        
-            <<< ButtonRow("Rows") {
-                $0.title = "Guardar"
-                $0.onCellSelection(didTapSaveButton)
-        }
-        
     }
     
     func didTapSaveButton(cell: ButtonCellOf<String>, row: ButtonRow) {
