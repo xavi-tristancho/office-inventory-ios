a82cbbd463909a1c65f6a9564b98a3fb2018c798
Xavier Tristancho <xavi@letitguide.com>
2016-03-24 20:33:57 +0100

    Added simple migration code

--- inventory/INV/Sync/INVSync.swift
+++ inventory/INV/Sync/INVSync.swift
@@ -11,8 +11,32 @@ import RealmSwift
 
 class INVSync: NSObject {
     
-    static func saveRecords(records : NSArray){
-        let uiRealm = try! Realm()
+    static let uiRealm = try! Realm();
+    
+    static func migrate () {
+        
+        let config = Realm.Configuration(
+            // Set the new schema version. This must be greater than the previously used
+            // version (if you've never set a schema version before, the version is 0).
+            schemaVersion: 1,
+            
+            // Set the block which will be called automatically when opening a Realm with
+            // a schema version lower than the one set above
+            migrationBlock: { migration, oldSchemaVersion in
+                // We haven’t migrated anything yet, so oldSchemaVersion == 0
+                if (oldSchemaVersion < 1) {
+                    // Nothing to do!
+                    // Realm will automatically detect new properties and removed properties
+                    // And will update the schema on disk automatically
+                }
+        })
+        
+        // Tell Realm to use this new configuration object for the default Realm
+        Realm.Configuration.defaultConfiguration = config            
+    }
+    
+    static func saveRecords(records : NSArray) {
+        
         do {            
             try uiRealm.write({ () -> Void in
                 for record in records{
@@ -24,11 +48,27 @@ class INVSync: NSObject {
         }
     }
     
-    static func readRecords(record: Object.Type) -> [Object]?{
-        
-        let uiRealm = try! Realm()
+    static func saveRecord(record: Object) {
+
+        do {
+            try uiRealm.write({ () -> Void in
+                uiRealm.add(record, update: true)
+            })
+        } catch {
+            print("DB Realm insert failed")
+        }
+    }
+    
+    static func readRecords(record: Object.Type) -> [Object]? {
         
         let records = uiRealm.objects(record)
+        
+        return Array(records)
+    }
+    
+    static func findRecords(record: Object.Type, filter: NSPredicate) -> [Object]? {
+        
+        let records = uiRealm.objects(record).filter(filter)
         
         return Array(records)
     }
